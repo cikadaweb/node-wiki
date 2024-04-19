e1b88c918c6f48a15cdc32ee0502afef93d732a3
garipov adel <a.garipov@okenit.ru>
2024-04-19 15:00:46 +0300

    Add handlebars

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
+import hbs from "hbs";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express TypeScript");
+    res.render("index", {
+        title: "File storage",
+        description: "List of stored files"
+    });
+});
+
+app.get("/upload", (req: Request, res: Response) => {
+    res.render("upload", {
+        title: "Upload files",
+    });
 });
 
 app.listen(port, () => {
