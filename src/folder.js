dc85ed53302d14447ec67c184369a9f7085e73d4
Adel <adel.garipovazn@mail.ru>
2024-07-17 16:48:44 +0300

    Downloaded files print

--- src/folder.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-
-let files_folder = [];
-fs.readdirSync('./files/').forEach((file) => {
-    files_folder.push(file)
-})
-
-console.log('files_folder: ', files_folder);
--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,25 @@ import path from "path";
 import hbs from "hbs";
 import multer from "multer";
 import fs from "fs";
+import { getFiles } from "./folder";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const filesDir = path.join(__dirname, '..', 'files');
+const filesDir = path.join(__dirname, "..", "files");
 if (!fs.existsSync(filesDir)) {
-    fs.mkdirSync(filesDir);
+  fs.mkdirSync(filesDir);
 }
 
 const storageConfig = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "files");
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname);
-    },
+  destination: (req, file, cb) => {
+    cb(null, "files");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
 const upload = multer({ storage: storageConfig });
@@ -30,32 +31,40 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.use("/files", express.static(filesDir));
 
 app.get("/", (req: Request, res: Response) => {
-    res.render("index", {
-        title: "File storage",
-        description: "List of stored files"
-    });
+  const files = getFiles("./files");
+  res.render("index", {
+    title: "File storage",
+    description: "List of stored files",
+    files,
+  });
 });
 
 app.get("/upload", (req: Request, res: Response) => {
-    res.render("upload", {
-        title: "Upload files",
-    });
+  res.render("upload", {
+    title: "Upload files",
+  });
 });
 
-app.post("/upload", upload.single("filedata"), (req: Request, res: Response, next: NextFunction) => {
+app.post(
+  "/upload",
+  upload.single("filedata"),
+  (req: Request, res: Response, next: NextFunction) => {
     const filedata = req.file;
     if (!filedata) {
-        res.send("Error file upload");
+      res.send("Error file upload");
     } else {
-        res.render("upload", {
-            title: "Upload files",
-        });
+      res.render("upload", {
+        title: "Upload files",
+      });
     }
-});
+  }
+);
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
