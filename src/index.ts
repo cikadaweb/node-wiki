c4d517bbd4c5afff700e739c0a4107a857ad09eb
Adel <adel.garipovazn@mail.ru>
2024-07-18 22:40:43 +0300

    Change hbs to express-handlebars

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import path from "path";
-import hbs from "hbs";
+import { engine } from "express-handlebars";
 import multer from "multer";
 import fs from "fs";
 import { getFiles } from "./folder";
@@ -11,14 +11,17 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const filesDir = path.join(__dirname, "..", "files");
+const publicDir = path.join(__dirname, "..", "public");
+
+const filesDir = path.join(publicDir, "files");
+
 if (!fs.existsSync(filesDir)) {
-  fs.mkdirSync(filesDir);
+  fs.mkdirSync(filesDir, { recursive: true });
 }
 
 const storageConfig = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "files");
+    cb(null, filesDir);
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
@@ -29,15 +32,21 @@ const upload = multer({ storage: storageConfig });
 
 app.use(express.urlencoded({ extended: true }));
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-hbs.registerPartials(__dirname + "/views/partials");
+// Настройка express-handlebars
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use("/files", express.static(filesDir));
+// Настройка промежуточного ПО для статических файлов
+app.use(express.static(publicDir));
 
 app.get("/", (req: Request, res: Response) => {
-  const files = getFiles("./files");
-  res.render("index", {
+  res.render('home', { title: 'Home' });
+});
+
+app.get("/filestorage", (req: Request, res: Response) => {
+  const files = getFiles(filesDir);
+  res.render("filestorage", {
     title: "File storage",
     description: "List of stored files",
     files,
@@ -51,19 +60,30 @@ app.get("/upload", (req: Request, res: Response) => {
 });
 
 app.post(
-  "/upload",
-  upload.single("filedata"),
-  (req: Request, res: Response, next: NextFunction) => {
-    const filedata = req.file;
-    if (!filedata) {
-      res.send("Error file upload");
-    } else {
-      res.render("upload", {
-        title: "Upload files",
-      });
+    "/upload",
+    upload.single("filedata"),
+    (req: Request, res: Response, next: NextFunction) => {
+      const filedata = req.file;
+      if (!filedata) {
+        res.send("Error file upload");
+      } else {
+        res.render("upload", {
+          title: "Upload files",
+        });
+      }
     }
-  }
 );
+
+app.use((req: Request, res: Response) => {
+  res.status(404);
+  res.render('404', { title: '404 Not Found' });
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.message);
+  res.status(500);
+  res.render('500');
+});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
