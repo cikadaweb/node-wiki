6fa732a08b6f2f5180222179e0d89db0a2e4bf85
Adel <adel.garipovazn@mail.ru>
2024-08-08 23:09:20 +0300

    Add body-parser, cookie-parser, session

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import path from 'path'
 import { engine } from 'express-handlebars'
 import multer from 'multer'
 import fs from 'fs'
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 import { getFiles } from './folder'
 import { getUniqueValues } from './helpers/getUniqueValues'
 
@@ -30,7 +33,23 @@ const storageConfig = multer.diskStorage({
 
 const upload = multer({ storage: storageConfig })
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.json()); // Использование body-parser для обработки JSON данных
+app.use(bodyParser.urlencoded({ extended: true })); // Использование body-parser для обработки данных формы
+
+if (!process.env.COOKIE_SECRET || !process.env.SESSION_SECRET) {
+    throw new Error('COOKIE_SECRET and SESSION_SECRET must be defined in the environment variables');
+}
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Set to true if using https
+}));
 
 // Настройка express-handlebars
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
@@ -77,13 +96,46 @@ app.post(
     },
 )
 
+app.get('/set-cookies', (req: Request, res: Response) => {
+    res.cookie('monster', 'ням-ням');
+    res.cookie('signed_monster', 'ням-ням', { signed: true });
+    res.send('Cookies are set');
+});
+
+app.get('/get-cookies', (req: Request, res: Response) => {
+    const monster = req.cookies.monster;
+    const signedMonster = req.signedCookies.signed_monster;
+    res.send(`Monster: ${monster}, Signed Monster: ${signedMonster}`);
+});
+
+app.get('/clear-cookies', (req: Request, res: Response) => {
+    res.clearCookie('monster');
+    res.send('Monster cookie cleared');
+});
+
+app.get('/set-session', (req: Request, res: Response) => {
+    (req.session as any).userName = 'Anonymous';
+    (req.session as any).colorScheme = 'dark';
+    res.send('Session variables are set');
+});
+
+app.get('/get-session', (req: Request, res: Response) => {
+    const userName = (req.session as any).userName;
+    const colorScheme = (req.session as any).colorScheme || 'dark';
+    res.send(`User Name: ${userName}, Color Scheme: ${colorScheme}`);
+});
+
+app.get('/clear-session', (req: Request, res: Response) => {
+    (req.session as any).userName = null;
+    delete (req.session as any).colorScheme;
+    res.send('Session variables are cleared');
+});
 app.use((req: Request, res: Response) => {
-    res.status(404)
-    res.render('404', { title: '404 Not Found' })
+    res.status(404).render('404', { title: '404 Not Found' })
 })
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => { // eslint-disable-line @typescript-eslint/no-unused-vars
-    console.error(err.message)
+    console.error('** ОШИБКА СЕРВЕРА: ' + err.message);
     res.status(500)
     res.render('500')
 })
